eab4348b04038bdb9cf8021f4b40d5c85ff01744
Sarthak Namdev <118155374+Srthk-08@users.noreply.github.com>
2024-06-26 12:59:28 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 // import logo from './logo.svg';
-import About from './components/About';
+// import About from './components/About';
 import './App.css';
 
 import React, { useState } from 'react';
@@ -8,12 +8,12 @@ import TextForm from './components/TextForm';
 import Alert from './components/Alert';
 
 
-import {
-  BrowserRouter as Router,
-  Route,
-  // Link,
-  Routes // switch ka replacement
-} from 'react-router-dom'; // switch ne work krna bnd kr diya
+// import {
+//   BrowserRouter as Router,
+//   Route,
+//   // Link,
+//   Routes // switch ka replacement
+// } from 'react-router-dom'; // switch ne work krna bnd kr diya
 
 function App() {
   const [mode, setMode] = useState('light');   // wheather dark mode is enabled or not
@@ -46,18 +46,20 @@ function App() {
 
   return (
     <>
-      <Router>
+      {/* <Router> */}
         <Navbar title="TextUtils" aboutText="About" mode={mode} toggleMode={toggleMode} />
         <Alert alert={alert} />
         <div className="container my-3">
-          <Routes>
-            <Route exact path="/about" element={<About />}> {/* exact path ko exactly match krta h */}
-            </Route>
-            <Route exact path="/" element={<TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />}>
-            </Route>
-          </Routes>
+          {/* <Routes> */}
+            {/* <Route exact path="/about" element={<About />}> exact path ko exactly match krta h */}
+            {/* </Route> */}
+            {/* <Route exact path="/" element= */}
+            <TextForm showAlert={showAlert} heading="Enter the text to analyze below" mode={mode} />
+           {/* }> */}
+           {/* </Route> */}
+          {/* </Routes> */}
         </div>
-      </Router>
+      {/* // </Router> */}
 
 
       {/* <Navbar title="TextUtils" aboutText="About" toggleMode={toggleMode} />
